5a2987f5d3624405abf2272286e310b2c4e9c434
Abhishek-chaurasiya <107196798+Abhishek-chaurasiya@users.noreply.github.com>
2022-09-13 22:16:27 +0530

    Create bellman-ford.cpp

---
+++ GraphCode/bellman-ford.cpp
@@ -0,0 +1,153 @@
+#include<bits/stdc++.h>
+#include<bitset>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+//template<typename T>
+ //using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define INF 1e18
+#define nline "\n"
+#define ll long long
+#define ull  unsigned long long
+#define lld  long double
+#define ld  long double;
+#define fr first
+#define sc second
+#define umap unordered_map
+#define uset unordered_set
+#define lb lower_bound
+#define ub upper_bound
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define all(v) (v).begin(),(v).end()
+#define all1(v) (v).begin()+1,(v).end()
+#define allr(v) (v).rbegin(),(v).rend()
+#define allr1(v) (v).rbegin()+1,(v).rend()
+#define sort0(v) sort(all(v))
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+//debug
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.fr); cerr << ","; _print(p.sc); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+#define sz(x) (ll)x.size()
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define mkp make_pair
+#define inf 100000000000
+const ll mod = 1e9 + 7;
+const int dx[9] = {1,0,-1,0,1,-1,-1,1,0};
+const int dy[9] = {0,1,0,-1,1,-1,1,-1,0};
+const int N=1e5 ;bitset<N> b;
+vector<ll> primes;
+void seive()
+{
+  b.set(); for(ll i=2;i<=N;i++)if(b[i]){ primes.pb(i);for(ll j=i*i;j<=N;j+=i)b[j]=0;}   
+}
+ll pwr(ll a, ll b) {a %= mod; ll res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct edge
+{
+	ll a,b,cost;
+};
+
+void solve()
+{
+ 
+  int n,m; cin>>n>>m;
+  std::vector<edge> e;
+  for(int i=0;i<m;i++){
+  	ll a,b,c; cin>>a>>b>>c;
+  	e.pb({a,b,c});
+  }
+  std::vector<ll> d(n+1,inf);
+
+  d[1]=0;
+  int x;
+  for(int i=0;i<n;i++)
+  {
+  	 x=-1;
+  	for(int j=0;j<m;j++)
+  	{
+  		if(d[e[j].a] <inf)
+  		{
+  			//Relaxing 
+  			if(d[e[j].a] + e[j].cost < d[e[j].b])
+  			{
+  				d[e[j].b]=min(d[e[j].b],d[e[j].a]+e[j].cost);
+  				x=e[j].b;
+  			}
+
+  		}
+       
+  	}
+  	debug(d);
+  }
+
+  
+
+  debug(x);
+  if(x!=-1)
+  {
+  	cout<<"There exist's a cycle.."<<endl;
+  }
+  else
+  for(int i=1;i<=n;i++)cout<<d[i]<<" "; 
+
+
+
+
+
+
+
+
+}
+
+
+int main(){
+ 
+ fast; 
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   freopen("Error.txt", "w", stderr);
+#endif
+
+ int tests=1;
+
+  while(tests--)
+  solve();
+
+
+}
+
