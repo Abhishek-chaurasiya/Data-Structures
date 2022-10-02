bf7cba078263d57df88ad21a1ff85246953c14e8
Abhishek-chaurasiya <107196798+Abhishek-chaurasiya@users.noreply.github.com>
2022-10-02 16:49:13 +0530

    Update Rolling hash function.cpp

--- String Algorithm/Rolling hash function.cpp
+++ String Algorithm/Rolling hash function.cpp
@@ -11,3 +11,67 @@
   return hash;
 }
 //GIVEN A STRING FIND THE NUMBER OF DIFFERENT SUBSTRING?
+//Brute force approach (n*n*n*log(n*k))
+//optimized O(n*n*log(n))
+int countsubstrings(string s){
+ 
+  set<int> ans;
+  for(int i=0;i<(int)s.size();i++){
+     
+     string sub="";
+     int hash=0,pr=1;
+     for(int j=i;j<(int)s.size();j++){
+       sub+=s[j];
+       hash=(hash+ (pr*(s[j]-'a'+1))%mod)%mod;
+       pr=(pr*31)%mod;
+       ans.insert(hash);
+     }
+
+  }
+  return ans.size();
+
+}
+
+
+//Given a string and Q queries ,every queries has 4 integers
+//(i1 j1) (i2 j2)
+//answer the query if substr(i1,j1) is equal to substr(i2,j2)
+
+//brute force 
+
+void solve(){
+ 
+ string s; cin>>s;
+ int q; cin>>q;
+ std::map<pii,int> mp;
+ int n=s.size();
+
+ for(int i=0;i<n;i++){
+  string str="";
+  
+  int pr=1,hash=0;
+  for(int j=i;j<n;j++){
+    str+=s[j];
+    hash=(hash+ (pr*(s[j]-'a'+1))%mod)%mod;
+    pr=(pr*31)%mod;
+    mp[{i,j}]=hash;
+  }
+
+ }
+
+
+ for(int i=0;i<q;i++){
+    int i1,j1,i2,j2; cin>>i1>>j1>>i2>>j2;
+    if(mp[{i1,j1}]==mp[{i2,j2}]){
+
+      cout<<"YES"<<endl;
+    }
+    else cout<<"NO"<<endl;
+ }  
+
+}
+
+//OPTIMIZED 
+
+
+
