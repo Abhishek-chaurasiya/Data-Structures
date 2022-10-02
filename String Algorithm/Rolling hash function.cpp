b0271892e42eb8cf7b2e599ff0a4bd011cf53031
Abhishek-chaurasiya <107196798+Abhishek-chaurasiya@users.noreply.github.com>
2022-10-03 00:47:08 +0530

    Update Rolling hash function.cpp

--- String Algorithm/Rolling hash function.cpp
+++ String Algorithm/Rolling hash function.cpp
@@ -13,6 +13,7 @@
 //GIVEN A STRING FIND THE NUMBER OF DIFFERENT SUBSTRING?
 //Brute force approach (n*n*n*log(n*k))
 //optimized O(n*n*log(n))
+//Instead of set<string> we are using rolling hash function set<integer>
 int countsubstrings(string s){
  
   set<int> ans;
@@ -72,6 +73,42 @@ void solve(){
 }
 
 //OPTIMIZED 
+
+vector<ll> prefixHash(N);
+ll computeSubstringHash(ll l,ll r){
+    ll hash=prefixHash[r];
+    if(l>0)hash=(hash-prefixHash[l-1]+mod)%mod;
+
+   //divides
+    hash=(hash*pwr(pwr(31,l),mod-2))%mod;
+
+    return hash;
+
+}
+void querySubstring(string s){
+
+   ll hash = 0 ,pr = 1;
+   ll n=s.size();
+   for(ll i=0;i<n;i++){
+     
+     hash=(hash+(pr*(s[i]-'a'+1))%mod)%mod;
+     prefixHash[i]=hash;
+     pr=(pr*31)%mod;
+   }
+
+   ll q; cin >> q;
+   while(q--){
+     ll i1,j1,i2,j2 ; cin>>i1>>j1>>i2>>j2;
+     
+     ll hash1=computeSubstringHash(i1,j1);
+     ll hash2=computeSubstringHash(i2,j2);
+
+     if(hash1==hash2) cout<< "YES" <<endl;
+     else cout<< "NO" <<endl;
+   }
+
+}
+
 
 
 
