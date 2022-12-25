41137f31f39a82fd2e1e4de7b5ec3005340cc3c3
Abhishek Chaurasiya <107196798+Abhishek-chaurasiya@users.noreply.github.com>
2022-12-26 01:00:20 +0530

    Update 2518. Number of Great Partitions.cpp

--- DP/2518. Number of Great Partitions.cpp
+++ DP/2518. Number of Great Partitions.cpp
@@ -9,12 +9,16 @@ public:
     int countPartitions(vector<int>& nums, int k) {
        
         int total = 1;
-        vector<int> dp(k,0); dp[0]=1;
+        vector<int> dp(k,0); 
+        // if we have nothing in our array can we make sum is equal to 0  -> answer is yes by taking nothing from array
+        dp[0]=1;
         long long sum = 0;
-        // for every 
+        // for every x in nums we create a single dp array 
+        // we think it as if we have x in our array can we make sum = (0 to k-1)
         for(auto &x:nums){
             for(int i=k-1;i>=0;i--){
                if(i-x>=0){
+                   // if we take x dp[i-x] is number of ways to 
                    dp[i]=(dp[i]+dp[i-x])%mod;
                }     
             }
