7c7688461847ced8374744f0ff29c2573fbce0d7
Abhishek Chaurasiya <107196798+Abhishek-chaurasiya@users.noreply.github.com>
2023-03-10 18:20:09 +0530

    Update Coin Combinations I.cpp

--- DP/CSES/Coin Combinations I.cpp
+++ DP/CSES/Coin Combinations I.cpp
@@ -28,3 +28,26 @@ void solve(){
 
 
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void solve(){
+  cin>>n>>target;
+  den.resize(n);
+  rep(i,0,n)cin>>den[i];
+ 
+  dp.resize(target+1,0);
+  // cout<<rec(target)<<endl;
+  // number of ways to make sum eqaul to 0 with a empty array is 1
+  dp[0] = 1;
+  for(int sum=0;sum<=target;sum++){
+    for(int i=0;i<n;i++){
+        if(sum >= den[i]){
+          dp[sum] = (dp[sum] + dp[sum-den[i]])%mod;
+        }
+    }
+    debug(dp);
+  }
+  cout<<dp[target]; 
+
+}
