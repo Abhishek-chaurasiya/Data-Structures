e3689c7a3046776823acc6a049e3e49fbb6cddb5
Abhishek Chaurasiya <107196798+Abhishek-chaurasiya@users.noreply.github.com>
2023-03-18 17:04:38 +0530

    Update Edit Distance.cpp

--- DP/CSES/Edit Distance.cpp
+++ DP/CSES/Edit Distance.cpp
@@ -1,4 +1,4 @@
-
+/////////////////////////////////////////////////////////////Top down/////////////////////////////////////////////////////////////////////////////////
 
     vector<vector<int>> dp;
     int helper(int i,int j,string &s1,string &s2){
@@ -18,23 +18,47 @@
 
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////bottom up////////////////////////////////////////////////////////////////////////////////////////////
+dp[i][j] is the minimum number of operation required to convert s1(0,i-1) to s2(0,j-1)
+if(s1[i] == s2[j]) dp[i][j] = dp[i-1][j-1]
+else 
+    dp[i][j] = 1 + dp[i-1][j] we delete i-1 the character and compare s1(0,i-2) with s2(0,j-1) which is already computed
+    dp[i][j] = 1 + dp[i][j-1] we insert j-1 the character and compare s1(0,i-1) with s2(0,j-2) which is already computed  
+    dp[i][j] = 1 + dp[i-1][j-1] we replace the character and compare s1(0,i-2) with s2(0,j-2) 
+    
+int minDistance(string A, string B) {
+        int M = A.size(), N = B.size();
+        vector<vector<int>> dp(M + 1, vector<int>(N + 1, INT_MAX));
+        for (int i = 0; i <= M; ++i) {
+            for (int j = 0; j <= N; ++j) {
+                if (i == 0 || j == 0) dp[i][j] = i + j;
+                else if (A[i - 1] == B[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = 1 + min({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] });
+            }
+        }
+        return dp[M][N];
+ }
+
+////////////////////////////////////space optimization //////////////////////////////////////////////////////////////////////////////////////////////////
  int minDistance(string s1, string s2) {
-         int n = s1.size() , m = s2.size();
-         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-         for(int i=n;i>=0;i--){
-             for(int j=m;j>=0;j--){
-                 if(i==n||j==m){
-                     dp[i][j] = (n-i)+(m-j);
-                 }
-                 else{
-                     // add the jth character to i
-                     dp[i][j]=1+dp[i][j+1];
-                     // remove the ith character
-                     dp[i][j]=min(dp[i][j],1+dp[i+1][j]);
-                     dp[i][j]=min(dp[i][j],dp[i+1][j+1]+(s1[i]==s2[j]?0:1));
-                 }
-             }
-         }
-        return dp[0][0];
+        int n = s1.size() , m = s2.size();
+        if(n < m){
+            swap(s1,s2);
+            swap(n,m);
+        }
+        vector<vector<int>> dp(2,vector<int>(m+1,INT_MAX));
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0)dp[i%2][j]=i+j;
+                else if(s1[i-1] == s2[j-1])dp[i%2][j] = dp[(i-1)%2][j-1];
+                else{
+                    dp[i%2][j]=1 + min({dp[(i-1)%2][j],dp[i%2][j-1],dp[(i-1)%2][j-1]});
+                }
+            }
+        }
+        return dp[n%2][m];
+        
     }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
