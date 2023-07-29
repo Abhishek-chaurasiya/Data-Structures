1cb0b3acfba553c861e69b2b3f916b75a4186b6b
Abhishek Chaurasiya <107196798+Abhishek-chaurasiya@users.noreply.github.com>
2023-07-29 12:58:06 +0530

    Update Diagonal Traversal of Binary Tree.cpp

--- Binary tree/Diagonal Traversal of Binary Tree.cpp
+++ Binary tree/Diagonal Traversal of Binary Tree.cpp
@@ -1,3 +1,24 @@
+Brute Force O(NlogN)
+void dfs(Node *root,int idx,map<int,vector<int>> &m){
+    if(!root)return;
+    m[idx].push_back(root->data);
+    dfs(root->left,idx+1,m);
+    dfs(root->right,idx,m);
+}
+
+vector<int> diagonal(Node *root)
+{
+   // your code here
+   map<int,vector<int>> m;
+   vector<int> ans;
+   dfs(root,0,m);
+   for(auto ob:m){
+       for(auto x:ob.second)ans.push_back(x);
+   }
+   return ans;
+}
+
+
 O(N) Approach
 vector<int> diagonal(Node *root)
 {
